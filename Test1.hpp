88d3fbe31434ef2e660d1a85525cdb8a2c0fb02f
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-06-11 15:01:31 +0200

    error handling

---
+++ Test1.hpp
@@ -0,0 +1,28 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   {FILENAME}.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafernan   <marvin@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/11 14:02:44 by mafernan         */+/*  */+/*            */
+/*   Updated: 2018/06/11 14:08:01 by mafernan        */#/* */#######.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef		TEST1_HPP
+/*define	TEST1_HPP
+
+class Test1 {
+	public:
+		Test1();
+		Test1(Test1 const & src);
+		Test1 operator=(Test1 const & src);
+		~Test1();
+	private:
+
+	protected:
+
+};
+
+#endif
